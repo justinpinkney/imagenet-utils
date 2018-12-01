af008b9225e88127c18da1340d338592d52a0ce8
Justin <justinpinkney@gmail.com>
2018-12-01 21:56:42 +0000

    use fixtures

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,39 @@ from .context import imagenet_utils as imnet
 import pytest
 import pathlib
 
+@pytest.fixture
+def two_urls(monkeypatch):
+    def mock_mapping(wnid):
+        return "first\turl1\nsecond\turl2\n"
+    monkeypatch.setattr(imnet.core, 'get_mapping', mock_mapping)
+    
+@pytest.fixture
+def bad_url(monkeypatch):
+    def mock_mapping(wnid):
+        return "Invalid url!\n"
+    monkeypatch.setattr(imnet.core, 'get_mapping', mock_mapping)
+
+@pytest.fixture
+def no_image_url(monkeypatch):
+    def mock_mapping(wnid):
+        return "\n"
+    monkeypatch.setattr(imnet.core, 'get_mapping', mock_mapping)
+
+@pytest.fixture
+def good_content_type(monkeypatch):
+    def mock_content_type(url):
+        return "image/jpeg"
+    monkeypatch.setattr(imnet.core, 'get_content_type', mock_content_type)
+
+@pytest.fixture
+def file_image_content(monkeypatch):
+    def mock_fetch(url):
+        test_image = pathlib.Path(__file__).with_name("test.jpg")
+        with open(test_image, "rb") as im_file:
+            image_content = im_file.read()
+        return image_content
+    monkeypatch.setattr(imnet.core, 'fetch_image', mock_fetch)
+
 def test_query_match():
     words = "this is a test of searching"
     query = "test of"
@@ -12,11 +45,7 @@ def test_search():
     result = imnet.search("taxus baccata")
     assert result[0].wnid == "n11661909"
 
-def test_urls(monkeypatch):
-    def mock_mapping(wnid):
-        return "first\turl1\nsecond\turl2\n"
-    monkeypatch.setattr(imnet.core, 'get_mapping', mock_mapping)
-
+def test_urls(two_urls):
     wnid = "n11661909"
     expected_length = 2
 
@@ -24,37 +53,20 @@ def test_urls(monkeypatch):
 
     assert len(urls) == expected_length
 
-def test_bad_urls(monkeypatch):
-    def mock_mapping(wnid):
-        return "Invalid url!\n"
-    monkeypatch.setattr(imnet.core, 'get_mapping', mock_mapping)
-
+def test_bad_urls(bad_url):
     not_wnid = "not_wnid"
      
     with pytest.raises(ValueError):
         urls = imnet.urls(not_wnid)
 
-def test_no_urls(monkeypatch):
-    def mock_mapping(wnid):
-        return "\n"
-    monkeypatch.setattr(imnet.core, 'get_mapping', mock_mapping)
+def test_no_urls(no_image_url):
 
     not_wnid = "no_urls"
      
     with pytest.raises(ValueError):
         urls = imnet.urls(not_wnid)
 
-def test_save_image(tmpdir, monkeypatch):
-    def mock_fetch(url):
-        test_image = pathlib.Path(__file__).with_name("test.jpg")
-        with open(test_image, "rb") as im_file:
-            image_content = im_file.read()
-        return image_content
-    def mock_content_type(url):
-        return "image/jpeg"
-    monkeypatch.setattr(imnet.core, 'fetch_image', mock_fetch)
-    monkeypatch.setattr(imnet.core, 'get_content_type', mock_content_type)
-
+def test_save_image(tmpdir, good_content_type, file_image_content):
     filename = "test"
     subdir = "folder"
     image = imnet.Image(filename, "")
